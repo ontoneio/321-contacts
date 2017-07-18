904fa78f68c2f7e52d914c22f9e65300ce4344f8
ontoneio <ontoneio.mm@gmail.com>
2017-07-18 13:35:50 -0700

    starts print function and sorts contacts in alphabetical order

--- 321-contacts.js
+++ 321-contacts.js
@@ -4,16 +4,21 @@ let allContacts = []
 
 const addContact = (firstName, lastName, email) => 
   allContacts.push([firstName, lastName, email])
-
+  
 const addContacts = (contactData) =>
   contactData.forEach((contact) => 
     addContact(contact.first_name, contact.last_name, contact.email));
 
+const printContacts = () =>
+  {const contacts = allContacts.map( (contact) =>
+    { return [ contact[0] + " " + contact[1], contact[2]]
+    }).sort((a, b) =>
+    {return a < b ? -1 : a > b ? 1 : 0}
+  )
+  console.log( "Loading Contact Data..." )
+  console.log( "..Finished Loading Contact Data");
+  console.log( contacts );
+  }
 
-const printContacts= function(contacts) {
-  console.log('printContacts:', 'TODO')
-}
-
-
-addContacts(contactsFile)
-console.log(allContacts);
+addContacts(contactsFile)  
+printContacts(allContacts)
